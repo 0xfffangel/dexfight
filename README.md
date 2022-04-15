87689aefa46f20c1f71cdc4152ec20bf31ca6849
0xfffangel <0xfffangel@protonmail.com>
2022-04-15 09:44:11 +0200

    1st version
    Signed-off-by: 0xfffangel <0xfffangel@protonmail.com>

---
+++ README.md
@@ -0,0 +1,36 @@
+DEX fight
+===
+
+Python price bot for multiple Dex.
+
+Based on [multidex-python](https://github.com/0xfffangel/multidex-python).
+
+### Install
+```bash
+python3 -m virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+### Example
+Example dexs fight on BSC chain
+```bash
+# BNB/BUSD
+python main.py --base BNB --input 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c --output 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56
+python main.py --base BNB --output 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56
+
+# BNB/USDT
+python main.py --base BNB --input 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c --output 0x55d398326f99059fF775485246999027B3197955
+python main.py --base BNB --output 0x55d398326f99059fF775485246999027B3197955
+
+# CAKE/BNB
+python main.py --base BNB --input 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82 --output 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c
+python main.py --base BNB --input 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82
+
+# MBOX/BNB
+python main.py --base BNB --input 0x3203c9E46cA618C8C1cE5dC67e7e9D75f5da2377 --output 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c
+python main.py --base BNB --input 0x3203c9E46cA618C8C1cE5dC67e7e9D75f5da2377
+
+# USDT/BUSD
+python main.py --base BNB --input 0x55d398326f99059fF775485246999027B3197955 --output 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56
+```
---
+++ main.py
@@ -0,0 +1,48 @@
+import asyncio
+import multidex
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--base", help = "Select base for chain")
+    parser.add_argument("-i", "--input", help = "Select input token address (default base address)")
+    parser.add_argument("-o", "--output", help = "Select output token address (default base address)")
+    return parser.parse_args()
+
+
+async def main():
+    args = get_arguments()
+    print("{:<12} {:<20} {:<20} {:<20}".format('Dex','Price','Reserve','Liquidity'))
+    values = {}
+    for dex in multidex.__all__:
+        if dex.base_symbol == args.base:
+            input = args.input if args.input is not None else dex.base_address
+            output = args.output if args.output is not None else dex.base_address
+            if dex.exist(input, output):
+                value = {
+                    'price': dex.price(input, output),
+                    'reserve_ratio': dex.reserve_ratio(input, output),
+                    'liquidity': dex.liquidity(input, output)
+                }
+                values[dex.platform] = value
+                print("{:<12} {:<20} {:<20} {:<20}".format(
+                    dex.platform, 
+                    value['price'],
+                    value['reserve_ratio'], 
+                    value['liquidity']))
+    print("")
+    header = list(values.keys())
+    header.insert(0, "")
+    format_row = "{:<12}" * (len(header)+1)
+    print(format_row.format("", *header))
+    for k, v in values.items():
+        gaps = [k]
+        for kk, vv in values.items():
+            gap = (v['reserve_ratio'] - vv['reserve_ratio']) / v['reserve_ratio']
+            gaps.append(round(gap, 6))
+        print(format_row.format("", *gaps))
+
+    
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
