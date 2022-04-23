3264d929b949478907e90bfe5ed02228b5944047
0xfffangel <0xfffangel@protonmail.com>
2022-04-23 08:18:10 +0200

    usage monitor with config file
    Signed-off-by: 0xfffangel <0xfffangel@protonmail.com>

--- monitor.py
+++ monitor.py
@@ -1,86 +1,105 @@
 import asyncio
 import multidex
-import argparse
 import datetime
 import calendar
-from os import path
+import sys
+import os
+import json
+import logging
 
-from pyrsistent import v
+class Config:
+    def __init__(self, base, input, output, routing, indecimals, outdecimals, min_gap):
+        self.base = base
+        self.input = input
+        self.output = output
+        self.routing = routing
+        self.indecimals = indecimals
+        self.outdecimals = outdecimals
+        self.min_gap = min_gap
+    
+    @staticmethod
+    def read(conf_file):
+        file = open(conf_file)
+        conf = file.read()
+        file.close()
+        res = json.loads(conf, object_hook=Config.from_json)
+        filename = os.path.basename(conf_file)
+        res.log_file = f"{filename}.log"
+        res.csv_file = f"{filename}.csv"
+        return res
 
-def get_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--base", help = "Select base for chain")
-    parser.add_argument("-i", "--input", help = "Select input token address (default base address)")
-    parser.add_argument("-o", "--output", help = "Select output token address (default base address)")
-    parser.add_argument("-r", "--routing", help = "Allow intermediate token address (default disabled, * = custom dex token)")
-    parser.add_argument("-j", "--indecimals", help = "Set input token decimals (default 18)")
-    parser.add_argument("-q", "--outdecimals", help = "Set output token decimals (default 18)")
-    return parser.parse_args()
+    @staticmethod
+    def from_json(json):
+      return Config(json['base'],
+                   json['input'], json['output'], json['routing'],
+                   json['indecimals'], json['outdecimals'], json['min_gap'])
 
 async def main():
-    args = get_arguments()
 
-    args.output = "0x818ec0a7fe18ff94269904fced6ae3dae6d6dc0b"
-    args.outdecimals = 6
-    args.base = "GLMR"
-    #args.routing = "*"
-    args.min_gap = 0.005
-    args.max_priceimpact = 0.01
+    #config_file = sys.argv[1]
+    config_file = "config.json"
+    conf = Config.read(config_file)
+    file_handler = logging.FileHandler(filename=conf.log_file)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    handlers = [file_handler, stdout_handler]
 
     values = {}
     for dex in multidex.__all__:
-        if dex.base_symbol == args.base:
-            input = args.input if args.input is not None else dex.base_address
-            output = args.output if args.output is not None else dex.base_address
-            if args.indecimals is not None:
-                dex.decimals(input, fallback = int(args.indecimals))
-            if args.outdecimals is not None:
-                dex.decimals(output, fallback = int(args.outdecimals))
+        if dex.base_symbol == conf.base:
+            input = conf.input if conf.input is not None else dex.base_address
+            output = conf.output if conf.output is not None else dex.base_address
+            if conf.indecimals is not None:
+                dex.decimals(input, fallback = int(conf.indecimals))
+            if conf.outdecimals is not None:
+                dex.decimals(output, fallback = int(conf.outdecimals))
             intermediate = None
             if dex.exist(input, output):
-                value = {
-                    'price': dex.price(input, output, intermediate),
-                    'reserves': dex.reserves(input, output, intermediate),
-                    'reserve_ratio': dex.reserve_ratio(input, output, intermediate),
-                    'liquidity': dex.liquidity(input, output)
-                }
+                value = dex_read(dex, input, output)
                 if value['reserves'][0] > 1 and value['reserves'][1] > 1:
                     values[dex.platform] = value
-
             intermediate = dex.token
             if dex.exist(input, output, intermediate):
-                value = {
-                    'price': dex.price(input, output, intermediate),
-                    'reserves': dex.reserves(input, output, intermediate),
-                    'reserve_ratio': dex.reserve_ratio(input, output, intermediate),
-                    'liquidity': dex.liquidity(input, output)
-                }
+                value = dex_read(dex, input, output, intermediate)
                 if value['reserves'][0] > 1 and value['reserves'][1] > 1:
                     values[dex.platform + "_dexcoin"] = value
 
     for k, v in values.items():
         for kk, vv in values.items():
             gap = (v['reserve_ratio'] - vv['reserve_ratio']) / v['reserve_ratio']
-            if gap !=0 and gap > args.min_gap:
+            if gap !=0 and gap > conf.min_gap:
                 now = datetime.datetime.now()
                 out = {
                     "timestamp": calendar.timegm(now.utctimetuple()),
                     'gap': gap
                 }
-                out = {**out, **dex_output(k, v)}
-                out = {**out, **dex_output(kk, vv)}
+                dex0 = dict(map(lambda kv: ('dex0_' + kv[0], kv[1]), v.items()))
+                dex1 = dict(map(lambda kv: ('dex1_' + kv[0], kv[1]), vv.items()))
+                out = {**out, **dex0}
+                out = {**out, **dex1}
                 list = out.values()
                 text = ', '.join([str(x) for x in list])
                 print(text)
+                appendcsv(conf.csv_file, out)
 
-def dex_output(platform, dex):
-    return {        
-        platform + '': platform,
-        platform + '_reserve_ratio': dex['reserve_ratio'],
-        platform + '_reserves0': dex['reserves'][0],
-        platform + '_reserves1': dex['reserves'][1],
-        platform + '_price': dex['price'] }
+def dex_read(dex, input, output, intermediate = None):
+    return {
+        'platform': dex.platform,
+        'price': dex.price(input, output, intermediate),
+        'reserves': dex.reserves(input, output, intermediate, refresh = True),
+        'reserve_ratio': dex.reserve_ratio(input, output, intermediate),
+        'liquidity_in': dex.liquidity_in(input, output, intermediate),
+        'liquidity_out': dex.liquidity_out(input, output, intermediate)
+    }
 
+def appendcsv(file, dict):
+    exist = os.path.exists(file)
+    file = open(file, 'a')
+    if not exist:
+        header = ', '.join([str(x) for x in dict.keys()])
+        file.write(header + '\r\n')
+    item = ', '.join([str(x) for x in dict.values()])
+    file.write(item + '\r\n')
+    file.close()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
