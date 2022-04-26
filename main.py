e8eb1070f4d83957e81b5ca9e303a8356d3793be
0xfffangel <0xfffangel@protonmail.com>
2022-04-27 00:17:13 +0200

    move base option to chain arg
    Signed-off-by: 0xfffangel <0xfffangel@protonmail.com>

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--base", help = "Select base for chain")
+    parser.add_argument("-c", "--chain", help = "Select chain")
     parser.add_argument("-i", "--input", help = "Select input token address (default base address)")
     parser.add_argument("-o", "--output", help = "Select output token address (default base address)")
     parser.add_argument("-r", "--routing", help = "Allow intermediate token address (default disabled, * = custom dex token)")
@@ -18,33 +18,32 @@ async def main():
 
     print("{:<12} {:<20} {:<20} {:<20} {:<20}".format('Dex','Price','Reserve','Liquidity In','Liquidity Out'))
     values = {}
-    for dex in multidex.__all__:
-        if dex.base_symbol == args.base:
-            input = args.input if args.input is not None else dex.base_address
-            output = args.output if args.output is not None else dex.base_address
-            intermediate = args.routing if args.routing is not None else None
-            intermediate = dex.token if args.routing == "*" else intermediate
+    for dex in multidex.all[args.chain]:
+        input = args.input if args.input is not None else dex.base_address
+        output = args.output if args.output is not None else dex.base_address
+        intermediate = args.routing if args.routing is not None else None
+        intermediate = dex.token if args.routing == "*" else intermediate
 
-            if dex.exist(input, output, intermediate):
-                if args.indecimals is not None:
-                    dex.decimals(input, fallback = int(args.indecimals))
-                if args.outdecimals is not None:
-                    dex.decimals(output, fallback = int(args.outdecimals))
-                if args.routedecimals is not None:
-                    dex.decimals(intermediate, fallback = int(args.routedecimals))
-                value = {
-                    'price': dex.price(input, output, intermediate),
-                    'reserve_ratio': dex.reserve_ratio(input, output, intermediate, refresh = True),
-                    'liquidity_in': dex.liquidity_in(input, output, intermediate),
-                    'liquidity_out': dex.liquidity_out(input, output, intermediate)
-                }
-                values[dex.platform] = value
-                print("{:<12} {:<20} {:<20} {:<20} {:<20}".format(
-                    dex.platform, 
-                    value['price'],
-                    value['reserve_ratio'],  
-                    value['liquidity_in'],
-                    value['liquidity_out']))
+        if dex.exist(input, output, intermediate):
+            if args.indecimals is not None:
+                dex.decimals(input, fallback = int(args.indecimals))
+            if args.outdecimals is not None:
+                dex.decimals(output, fallback = int(args.outdecimals))
+            if args.routedecimals is not None:
+                dex.decimals(intermediate, fallback = int(args.routedecimals))
+            value = {
+                'price': dex.price(input, output, intermediate),
+                'reserve_ratio': dex.reserve_ratio(input, output, intermediate, refresh = True),
+                'liquidity_in': dex.liquidity_in(input, output, intermediate),
+                'liquidity_out': dex.liquidity_out(input, output, intermediate)
+            }
+            values[dex.platform] = value
+            print("{:<12} {:<20} {:<20} {:<20} {:<20}".format(
+                dex.platform, 
+                value['price'],
+                value['reserve_ratio'],  
+                value['liquidity_in'],
+                value['liquidity_out']))
     print("")
     header = list(values.keys())
     header.insert(0, "")
